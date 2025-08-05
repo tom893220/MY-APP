1cdd8371ea708ec65129d9166b781c79f5723f90
tom893220 <34365104+tom893220@users.noreply.github.com>
2025-08-05 22:29:39 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+/* https://drive.google.com/drive/folders/1wcdnBSRJnsleKTs5A96vnjSHJpg8oq8i?usp=sharing */
+/* https://docs.google.com/document/d/1X9DTlFcW4Le15fUQMNSvTRVm-lROSpM-HL1tyXBvbfs/edit?usp=drivesdk */
+/* ttps://docs.google.com/document/d/1B71EWWeQEWAoeg6SYm06a3PEtV5Nr8fScWXebEHmYZs/edit?usp=drivesdk */
+/* https://docs.google.com/document/d/1bcJC8H5o51EitCbN8Fi6oE7Njr2cKa1Ic6hxN5NNEzY/edit?usp=drivesdk */
+/* https://docs.google.com/document/d/1CeWelUbKW2OxrVli23YRSodsocrd-thI3EvkBrdunO8/edit?usp=drivesdk */
 export default nextConfig;
